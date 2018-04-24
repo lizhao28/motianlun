647ff02dfec216d513c6069b7f02813b270f2eeb
lizhao <lizhao54@qq.com>
2018-04-24 15:56:34 +0800

    o.1`

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './stylesheets/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        1
+      </div>
+    );
+  }
+}
+
+export default App;
